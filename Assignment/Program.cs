40fc512d5e1fd8da791567260b8dfe6b838c1062
bi80ib <bi80ib@student.sunderland.ac.uk>
2025-10-08 10:56:29 +0100

    PlayerArray Created

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,9 @@ namespace Assignment
         static void Main(string[] args)
         {
 
-
+            Player[] players = new Player[3];
+            PlayerArray a1 = new PlayerArray();
+            
 
         }
 
@@ -20,11 +23,39 @@ namespace Assignment
             public int hoursPlayed { get; set; }
             public int highScore { get; set; }
 
-            public Player(int hoursPlayed, int highScore)
+            public int id { get; set; }
+
+            public string username { get; set; }
+
+            public Player(int hoursPlayed, int highScore, int id, string username)
             {
                 this.hoursPlayed = hoursPlayed;
                 this.highScore = highScore;
+                this.id = id;
+                this.username = username;
             }
+        }
+
+        class PlayerArray
+        {
+
+            public void addPlayer(Player player, Player[] players)
+            {
+                for (int i = 0; i < players.Length; i++) 
+                {
+                    if(players[i] != null)
+                    {
+                        players[i] = player;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Full");
+                    }
+                }
+
+
+
+            
         }
     }
 }
