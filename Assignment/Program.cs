9a466f09e9ca301918ba87b1a93a7313f9c61b2b
bi80ib <bi80ib@student.sunderland.ac.uk>
2025-10-08 10:46:27 +0100

    Player Class Created

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -10,9 +10,21 @@ namespace Assignment
     {
         static void Main(string[] args)
         {
-        
-            
-        
+
+
+
+        }
+
+        class Player
+        {
+            public int hoursPlayed { get; set; }
+            public int highScore { get; set; }
+
+            public Player(int hoursPlayed, int highScore)
+            {
+                this.hoursPlayed = hoursPlayed;
+                this.highScore = highScore;
+            }
         }
     }
 }
