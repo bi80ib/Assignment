1d3708927655eb96c2177b9b0aa6089865a21271
bi80ib <bi80ib@student.sunderland.ac.uk>
2025-10-15 10:39:27 +0100

    Added a search bu username and by id function abd created a text file which usernames and ids are added into and then read from

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
+    
+
 
 namespace Assignment
 {
@@ -15,7 +19,25 @@ namespace Assignment
 
             Player[] players = new Player[3];
             PlayerArray a1 = new PlayerArray();
+            string file = "Record.txt";
+
+
             
+
+            if (File.Exists(file))
+            {
+                Console.WriteLine("File exists");
+            }
+
+            using (StreamWriter sw = new StreamWriter(file))
+            {
+                
+                sw.WriteLine("Hello");
+                sw.Close();
+            }
+
+
+
             int repeat = 0;
             while (repeat == 0)
             {
@@ -25,7 +47,7 @@ namespace Assignment
                 int choice = Convert.ToInt32(Console.ReadLine());
                 if (choice == 1)
                 {
-                    a1.AddPlayer(players);
+                    a1.AddPlayer(players,file);
                 }
                 else if (choice == 2)
                 {
@@ -33,7 +55,7 @@ namespace Assignment
                 }
                 else if (choice == 3)
                 {
-                    a1.PrintArray(players);
+                    a1.PrintArray(players, file);
                 }
             }
 
@@ -62,28 +84,43 @@ namespace Assignment
                 this.username = username;
             }
 
-            public Player(int id)
+            public Player(int id,string username)
             {
                 hoursPlayed = 0;
                 highScore = 0;
                 this.id = id;
-                username = "";
+                this.username = username;
 
 
+            }
+
+            public override string ToString()
+            {
+                return $"ID: {id}, Username: {username}, Hours Played: {hoursPlayed}, High Score: {highScore}";
             }
         }
 
         class PlayerArray
         {
 
-            public void AddPlayer( Player[] players)
+            public void AddPlayer( Player[] players, string file)
             {
+               Console.WriteLine("Enter username");
+                string username = Console.ReadLine();
+
                 for (int i = 1; i < players.Length; i++)
                 {
                     if (players[i] == null)
                     {
-                        players[i] = new Player(i);
+                        players[i] = new Player(i,username);
+                        using (StreamWriter sw = new StreamWriter(file))
+                        {
+                            sw.WriteLine(players[i]);
+                            sw.WriteLine("Hello");
+                            sw.Close();
+                        }
                         break;
+
                     }
                     else 
                     {
@@ -96,32 +133,62 @@ namespace Assignment
 
             public void SearchArray(Player[] players)
             {
-                Console.WriteLine("Enter id");
-                
+                Console.WriteLine("Enter 1:id or 2:username");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if(choice == 1)
+                {
+                    SearchByID(players);
+                }
+                else if (choice == 2)
+                {
+                    SearchByUsername(players);
+                }
+                                                              
+            }
+
+            public void SearchByID(Player[] players)
+            {
+                Console.WriteLine("Enter ID");
                 int checkID = Convert.ToInt32(Console.ReadLine());
                 foreach (Player player in players)
                 {
-                    if(player != null && player.id == checkID)
+                    if (player != null && player.id == checkID)
                     {
-                      
                         Console.WriteLine(player.id);
-                      
                     }
-
-                    Console.WriteLine("Not found");
-                   
-                    
+                    else
+                    {
+                        Console.WriteLine("Not found");
+                    }
                 }
-                
             }
-            
-            public void PrintArray(Player[] players)
+
+            public void SearchByUsername(Player[] players)
+            {
+                Console.WriteLine("Enter username");
+                string checkUsername = Console.ReadLine();
+                foreach (Player player in players)
+                {
+                    if (player != null && player.username == checkUsername)
+                    {
+                        Console.WriteLine(player.username);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not found");
+                    }
+                }
+            }
+
+            public void PrintArray(Player[] players,string file)
             {
                 foreach (Player player in players)
                 {
                     if (player != null)
                     {
-                        Console.WriteLine(player.id);
+                        Console.WriteLine(player);
+                        
+
                     }
                 }
             }
